b954b751e1b98f223b287c5bd17f06e3ca5c6223
zackcaussy <zackcaussy@gmail.com>
2022-10-24 11:58:42 +0200

    pull

---
+++ Info-et-Contacts/js/script.js
@@ -0,0 +1,8 @@
+<script>
+    const menuHamburger = document.querySelector(".menu-burger")
+    const navLinks = document.querySelector(".nav-links")
+
+    menuHamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('mobile-menu')
+    })
+</script>
