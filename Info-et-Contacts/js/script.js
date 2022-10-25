f91567319c26f8642f265244ae20b78c0c9d181f
caussy zack <zackcaussy@gmail.com>
2022-10-25 23:27:10 +0200

    push

--- Info-et-Contacts/js/script.js
+++
@@ -1,8 +0,0 @@
-<script>
-    const menuHamburger = document.querySelector(".menu-burger")
-    const navLinks = document.querySelector(".nav-links")
-
-    menuHamburger.addEventListener('click', () => {
-        navLinks.classList.toggle('mobile-menu')
-    })
-</script>
--- Site-Final/Info et Contacts/burger.js
+++
@@ -1,7 +0,0 @@
-
-    const menuHamburger = document.querySelector(".menu-burger")
-    const navLinks = document.querySelector(".nav-links")
-
-    menuHamburger.addEventListener('click', () => {
-        navLinks.classList.toggle('mobile-menu')
-    })
